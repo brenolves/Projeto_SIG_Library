3a8d69fed83fa5154b826cdda505b3888c08514f
Breno <brenoalves43@hotmail.com>
2021-01-22 15:45:18 -0300

    Modifiquei o programa para deixá-lo mais original.

--- siglibrary.c
+++ siglibrary.c
@@ -1,12 +1,3 @@
-///////////////////////////////////////////////////////////////////////////////
-///             Universidade Federal do Rio Grande do Norte                 ///
-///                Centro de Ensino Superior do Seridó                      ///
-///               Departamento de Computação e Tecnologia                   ///
-///                  Disciplina DCT1106 -- Programação                      ///
-///               Projeto Sistema de Controle de Biblioteca                 ///
-///                Desenvolvido por @brenolves | 01/2021                    ///
-///////////////////////////////////////////////////////////////////////////////
-
 #include <stdio.h>
 
 void menuPrincipal(void);
@@ -22,22 +13,21 @@ void menuPrincipal(void) {
     printf("\n");
     printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
     printf("|||                                                                         |||\n");
-    printf("|||             Universidade Federal do Rio Grande do Norte                 |||\n");
-    printf("|||                 Centro de Ensino Superior do Seridó                     |||\n");
-    printf("|||               Departamento de Computação e Tecnologia                   |||\n");
-    printf("|||                  Disciplina DCT1106 -- Programação                      |||\n");
-    printf("|||              Projeto Sistema de Controle de Biblioteca                  |||\n");
-    printf("|||               Desenvolvido por @brenolves | 01/2021                     |||\n");
-    printf("|||                                                                         |||\n");
-    printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
-    printf("|||                                                                         |||\n");
-    printf("|||            = = = = = Sistema de Controle de Biblioteca = = = = =        |||\n");
+    printf("|||               =||=||=||=||=|| SIG-Library ||=||=||=||=||=               |||\n");
     printf("|||                                                                         |||\n");
     printf("|||  Projeto criado no intuito de prover um melhor gerenciamento de         |||\n");
     printf("|||  bibliotecas, gerindo o cadastro de novos livros, empréstimos de livros,|||\n");
     printf("|||  e os usuários da biblioteca, podendo também navegar pelas datas de em- |||\n");
     printf("|||  -préstimo, prazos de devolução, livros disponíveis e usuários          |||\n");
     printf("|||  cadastrados.                                                           |||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||                            Menu Principal                               |||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||                                                                         |||\n");
+    printf("|||                                                                         |||\n");
     printf("|||                                                                         |||\n");
     printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
     printf("\n");
