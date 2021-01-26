d19a86d0b799428530bfddb346a885a0def32f34
Breno <brenoalves43@hotmail.com>
2021-01-26 18:32:41 -0300

    Adicionadas as telas dos menús de Livros, Usuários e Empréstimos. E também uma função para limpar a tela com mais eficiência.

--- siglibrary.c
+++ siglibrary.c
@@ -1,34 +1,158 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void menuPrincipal(void);
+void menuAbout(void);
+void menuLivros(void);
+void menuUsuarios(void);
+void menuEmprestimos(void);
+void clearscr(void);
 
 int main(void) {
     menuPrincipal();
+    menuAbout();
+    menuLivros();
+    menuUsuarios();
+    menuEmprestimos();
     return 0;
 
 }
 
+// Função pra limpar a tela que funciona em vários SO's.
+
+void clearscr(void) {
+
+#ifdef _WIN32
+    clearscr();
+#elif defined(unix) || defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))
+    system("clear");
+#else
+    #error "Sistema operacional não suportado."
+#endif
+
+}
 
 void menuPrincipal(void) {
-    printf("\n");
-    printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
-    printf("|||                                                                         |||\n");
-    printf("|||               =||=||=||=||=|| SIG-Library ||=||=||=||=||=               |||\n");
-    printf("|||                                                                         |||\n");
-    printf("|||  Projeto criado no intuito de prover um melhor gerenciamento de         |||\n");
-    printf("|||  bibliotecas, gerindo o cadastro de novos livros, empréstimos de livros,|||\n");
-    printf("|||  e os usuários da biblioteca, podendo também navegar pelas datas de em- |||\n");
-    printf("|||  -préstimo, prazos de devolução, livros disponíveis e usuários          |||\n");
-    printf("|||  cadastrados.                                                           |||\n");
-    printf("|||                                                                         |||\n");
-    printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
-    printf("|||                                                                         |||\n");
-    printf("|||                                                                         |||\n");
-    printf("|||                            Menu Principal                               |||\n");
-    printf("|||                                                                         |||\n");
-    printf("|||                                                                         |||\n");
-    printf("|||                                                                         |||\n");
-    printf("|||                                                                         |||\n");
-    printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
-    printf("\n");
+    clearscr();
+        printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||              =||=||=||=||=|| SIG-Library ||=||=||=||=||=                |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                            Menu Principal                               |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                    |#| 1 - Livros                                       |||\n");
+        printf("|||                    |#| 2 - Usuários                                     |||\n");
+        printf("|||                    |#| 3 - Empréstimos                                  |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+        printf("|||        4 - Sobre o programa         |:|          5 - Encerrar           |||\n");
+        printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+        printf("\n");
+        printf("||| Aperte qualquer tecla para continuar...\n");
+        getchar();
+}
+
+void menuAbout(void) {
+    clearscr();
+        printf("\n");
+        printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||               =||=||=||=||=|| SIG-Library ||=||=||=||=||=               |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||  Projeto criado no intuito de prover um melhor gerenciamento de         |||\n");
+        printf("|||  bibliotecas, gerindo o cadastro de novos livros, empréstimos de livros,|||\n");
+        printf("|||  e os usuários da biblioteca, podendo também navegar pelas datas de em- |||\n");
+        printf("|||  -préstimo, prazos de devolução, livros disponíveis e usuários          |||\n");
+        printf("|||  cadastrados.                                                           |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||     Desenvolvido por @brenolves                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+        printf("\n");
+        printf("||| Aperte qualquer tecla para continuar...\n");
+        getchar();
+
+}
+
+void menuLivros(void) {
+    clearscr();
+        printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||              =||=||=||=||=|| SIG-Library ||=||=||=||=||=                |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                              Menu Livros                                |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                    |#| 1 - Cadastrar um livro                           |||\n");
+        printf("|||                    |#| 2 - Pesquisar um livro                           |||\n");
+        printf("|||                    |#| 3 - Atualizar cadastro de um livro               |||\n");
+        printf("|||                    |#| 4 - Excluir cadastro de um livro                 |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+        printf("|||        4 - Menu Principal           |:|          5 - Encerrar           |||\n");
+        printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+        printf("\n");
+        printf("||| Aperte qualquer tecla para continuar...\n");
+        getchar();
+}
+
+void menuUsuarios(void) {
+    clearscr();
+        printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||              =||=||=||=||=|| SIG-Library ||=||=||=||=||=                |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                             Menu Usuarios                               |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                    |#| 1 - Cadastrar um usuario                         |||\n");
+        printf("|||                    |#| 2 - Pesquisar um usuario                         |||\n");
+        printf("|||                    |#| 3 - Atualizar cadastro de um usuario             |||\n");
+        printf("|||                    |#| 4 - Excluir cadastro de um usuario               |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+        printf("|||        4 - Menu Principal           |:|          5 - Encerrar           |||\n");
+        printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+        printf("\n");
+        printf("||| Aperte qualquer tecla para continuar...\n");
+        getchar();
+}
+
+void menuEmprestimos(void) {
+    clearscr();
+        printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||              =||=||=||=||=|| SIG-Library ||=||=||=||=||=                |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                           Menu Empréstimos                              |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                    |#| 1 - Efetuar um empréstimo                        |||\n");
+        printf("|||                    |#| 2 - Pesquisar um empréstimo                      |||\n");
+        printf("|||                    |#| 3 - Atualizar cadastro de um usuario             |||\n");
+        printf("|||                    |#| 4 - Excluir cadastro de um usuario               |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||                                                                         |||\n");
+        printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+        printf("|||        4 - Menu Principal           |:|          5 - Encerrar           |||\n");
+        printf("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+        printf("\n");
+        printf("||| Aperte qualquer tecla para continuar...\n");
+        getchar();
 }
