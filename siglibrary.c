28bd811a719d6f4968c8c60716f0af9d52999620
Breno <brenoalves43@hotmail.com>
2021-01-26 19:31:47 -0300

    Adicionei o link de onde peguei a função clearscr no comentário.

--- siglibrary.c
+++ siglibrary.c
@@ -18,7 +18,7 @@ int main(void) {
 
 }
 
-// Função pra limpar a tela que funciona em vários SO's.
+// Função pra limpar a tela que funciona em vários SO's. Encontrei neste site >> https://stackoverflow.com/questions/18154579/how-do-i-clear-the-screen-in-c/18154598
 
 void clearscr(void) {
 
